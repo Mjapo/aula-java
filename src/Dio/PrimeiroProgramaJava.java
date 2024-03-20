3eb27581a0cdf145ac31ad6be06b7b9e1eb0bc58
Mjapo <marceloishida14@gmail.com>
2024-03-20 18:17:58 -0300

    first commit

---
+++ src/Dio/PrimeiroProgramaJava.java
@@ -0,0 +1,15 @@
+package Dio;
+import Dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
+
+
+
+
+
